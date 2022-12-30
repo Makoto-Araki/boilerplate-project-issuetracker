9150faf8b73d9eb7d0ac5012cfa04fd24346719b
MakotoAraki <85997616+Makoto-Araki@users.noreply.github.com>
2022-12-30 10:59:01 +0000

    2022年12月30日 - 07

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,5 +6,36 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
+
+  // Timeout 20 seconds
+  this.timeout(20000);
+
+  // POST - URL/api/issues/:project
+  test('Create an issue with every field', (done) => {
+    chai
+      .request(server)
+      .post('/api/issues/project1')
+      .send({
+        issue_title: 'Bug-001',
+        issue_text: 'Bug-001 text',
+        created_by: 'admin',
+        assigned_to: 'makoto',
+        status_text: 'fixing',
+      })
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.equal(res.type, 'application/json');
+        assert.isNotNull(res.body._id);
+        assert.equal(res.body.issue_title, 'Bug-001');
+        assert.equal(res.body.issue_text, 'Bug-001 text');
+        assert.equal(res.body.created_by, 'admin');
+        assert.equal(res.body.assigned_to, 'makoto');
+        assert.isBoolean(res.body.open);
+        assert.equal(res.body.status_text, 'fixing');
+        assert.isNotNull(res.body.created_on);
+        assert.isNotNull(res.body.updated_on);
+        done();
+      });
+  });
   
 });

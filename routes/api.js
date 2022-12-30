a014150d8d20ea54f70949b6d7d94be4ff87f1d1
MakotoAraki <85997616+Makoto-Araki@users.noreply.github.com>
2022-12-30 01:32:17 +0000

    2022年12月30日 - 03

--- routes/api.js
+++ routes/api.js
@@ -2,34 +2,15 @@
 
 // Import & Config
 const mongoose = require('mongoose');
-const parser = require('body-parser');
-require('dotenv').config();
 
-// Secrets
-const user = process.env['user']
-const pass = process.env['pass']
-const option = process.env['option']
-const database = process.env['database']
-
-// Mongo_URI
-const mongo_URI = `mongodb+srv://${user}:${pass}@$cluster0.snt16.mongodb.net/${database}?${option}`;
-  
-// MongoDB Connect Config
-mongoose.set('strictQuery', false);
-
-// MongoDB Connect
-mongoose
-.connect(mongo_URI)
-.then(() => console.log('Database connection successed'))
-.catch((err) => console.error(err))
 
 // Schema
 const projectSchema = new mongoose.Schema({
   issue_title: { type: String },
   issue_text: { type: String },
-  assined_to: { type: String },
   created_by: { type: String },
-  open: { type: Boolean },
+  assigned_to: { type: String },
+  open: { type: Boolean, default: true },
   status_text: { type: String },
   created_on: { type: Date, default: Date.now },
   updated_on: { type: Date, default: Date.now },
@@ -42,12 +23,37 @@ module.exports = function (app) {
       let project = req.params.project;
       //
     })
-    
+
+    // POST - URL/api/issues/:project
     .post(function (req, res){
+      
+      // Model and collection are made under the name of req.params.project
       let project = req.params.project;
       let Project = mongoose.model(project, projectSchema);
       let entry = new Project();
+
+      // Form data is set in document
       entry.issue_title = req.body.issue_title;
+      entry.issue_text = req.body.issue_text;
+      entry.created_by = req.body.created_by;
+      entry.assigned_to = req.body.assigned_to;
+      entry.status_text = req.body.status_text;
+
+      // Document is going to be saved
+      entry.save((err, doc) => {
+        if (!err) {
+          res.json({
+            _id: doc._id,
+            issue_title: doc.issue_title,
+            issue_text: doc.issue_text,
+            created_by: doc.created_by,
+            assigned_to: doc.assigned_to,
+            status_text: doc.status_text,
+          });
+        } else {
+          console.error(err);
+        }
+      });
     })
     
     .put(function (req, res){
--- server.js
+++ server.js
@@ -1,52 +1,72 @@
 'use strict';
 
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const expect      = require('chai').expect;
-const cors        = require('cors');
+// Import & Config
+const express = require('express');
+const bodyParser = require('body-parser');
+const expect = require('chai').expect;
+const cors = require('cors');
+const mongoose = require('mongoose');
 require('dotenv').config();
 
-const apiRoutes         = require('./routes/api.js');
-const fccTestingRoutes  = require('./routes/fcctesting.js');
-const runner            = require('./test-runner');
+// Load Modules
+const apiRoutes = require('./routes/api.js');
+const fccTestingRoutes = require('./routes/fcctesting.js');
+const runner = require('./test-runner');
 
+// App initialize
 let app = express();
 
+// App config
 app.use('/public', express.static(process.cwd() + '/public'));
-
 app.use(cors({origin: '*'})); //For FCC testing purposes only
-
-
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//Sample front-end
+// Secrets
+const user = process.env['user']
+const pass = process.env['pass']
+const cluster = process.env['cluster']
+const option = process.env['option']
+const database = process.env['database']
+
+// Mongo_URI
+const mongo_URI = `mongodb+srv://${user}:${pass}@${cluster}/${database}?${option}`;
+  
+// MongoDB Connect Config
+mongoose.set('strictQuery', false);
+
+// MongoDB Connect
+mongoose
+.connect(mongo_URI)
+.then(() => console.log('Database connection successed'))
+.catch((err) => console.error(err))
+
+// Sample front-end
 app.route('/:project/')
   .get(function (req, res) {
     res.sendFile(process.cwd() + '/views/issue.html');
   });
 
-//Index page (static HTML)
+// Index page (static HTML)
 app.route('/')
   .get(function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
   });
 
-//For FCC testing purposes
+// For FCC testing purposes
 fccTestingRoutes(app);
 
-//Routing for API 
+// Routing for API 
 apiRoutes(app);  
     
-//404 Not Found Middleware
+// 404 Not Found Middleware
 app.use(function(req, res, next) {
   res.status(404)
     .type('text')
     .send('Not Found');
 });
 
-//Start our server and tests!
+// Start our server and tests!
 const listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
   if(process.env.NODE_ENV==='test') {

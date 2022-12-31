289e5e1dee979721ce989ee4c15a8cb1d43838f8
MakotoAraki <85997616+Makoto-Araki@users.noreply.github.com>
2022-12-31 09:50:49 +0000

    2022年12月31日 - 03

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -64,6 +64,57 @@ suite('Functional Tests', function() {
       });
   });
 
+  // POST - URL/api/issues/:project
+  test('Create an issue with missing required fields', (done) => {
+    chai
+      .request(server)
+      .post('/api/issues/project1')
+      .send({
+        assigned_to: 'makoto',
+        status_text: 'fixing',
+      })
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.equal(res.type, 'application/json');
+        assert.equal(res.body.error, 'required field(s) missing');
+        done();
+      });
+  });
+
   // GET - URL/api/issues/:project
+  test('View issues on a project', (done) => {
+    chai
+      .request(server)
+      .get('/api/issues/project2')
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.equal(res.type, 'application/json');
+        assert.equal(res.body.length, 4);
+        assert.deepEqual(res.body[0], {
+          _id: '63afb4acf9b116416156dfa8',
+          issue_title: 'Bug-001',
+          issue_text: 'Bug-001 text',
+          created_by: 'admin',
+          assigned_to: 'makoto',
+          open: true,
+          status_text: 'fixing',
+          created_on: '2022-12-31T04:03:56.124+00:00',
+          updated_on: '2022-12-31T04:03:56.124+00:00',
+        });
+        assert.deepEqual(res.body[1], {
+          _id: '63afb4c8f9b116416156dfaa',
+          issue_title: 'Bug-002',
+          issue_text: 'Bug-002 text',
+          created_by: 'admin',
+          assigned_to: 'takeshi',
+          open: false,
+          status_text: 'fixed',
+          created_on: '2022-12-31T04:04:24.789+00:00',
+          updated_on: '2022-12-31T04:12:16.176+00:00',
+        });
+        done();
+      });
+  });
+  
 */
 });
